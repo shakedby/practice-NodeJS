48ebdf9019eedc276bee2be2ed7a3565bd54eb76
shaked ben yechezkel <76395111+shakedby@users.noreply.github.com>
2021-10-24 16:01:03 +0300

    24/10

--- util/location.js
+++ util/location.js
@@ -1,4 +1,5 @@
 //const API_KEY=....;
+//const axios = require("axios");
 
 async function getCoordsForAddress(address) {
   return {
@@ -6,3 +7,42 @@ async function getCoordsForAddress(address) {
     lng: -73.9856644,
   };
 }
+
+module.exports = getCoordsForAddress;
+//********************************************************************/
+
+//====> real api::::
+
+// const axios = require('axios');
+
+// const HttpError = require('../models/http-error');
+
+// const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+
+// async function getCoordsForAddress(address) {
+//   // return {
+//   //   lat: 40.7484474,
+//   //   lng: -73.9871516
+//   // };
+//   const response = await axios.get(
+//     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+//       address
+//     )}&key=${API_KEY}`
+//   );
+
+//   const data = response.data;
+
+//   if (!data || data.status === 'ZERO_RESULTS') {
+//     const error = new HttpError(
+//       'Could not find location for the specified address.',
+//       422
+//     );
+//     throw error;
+//   }
+
+//   const coordinates = data.results[0].geometry.location;
+
+//   return coordinates;
+// }
+
+// module.exports = getCoordsForAddress;
